23ce2ec9fc3b00ef3814f2965eea674904c1ed6a
AhmetEnes2021 <84579324+AhmetEnes2021@users.noreply.github.com>
2023-03-17 12:58:30 +0300

    asdasda

---
+++ FNGParticleSystem_/Assets/FireFly/ButterFlySpawnScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterFlySpawnScript : MonoBehaviour
+{
+    GameObject ButterFlyPrefab;
+    [SerializeField] private float SpawnRadius;
+    [SerializeField] private List<int> Count = new List<int>();
+    [SerializeField] private LayerMask GroundLayer;
+
+
+    RaycastHit hit;
+    float SpawnX, SpawnZ, SpawnY;
+    int timer;
+
+    private void FixedUpdate()
+    {
+
+        SpawnX = (transform.position.x + Random.Range(-SpawnRadius, SpawnRadius + 1));
+        SpawnZ = (transform.position.z + Random.Range(-SpawnRadius, SpawnRadius + 1));
+        if (Count.Count < 10)
+        {
+            timer++;
+            if (timer > 50)
+            {
+                if(Physics.Raycast(new Vector3(SpawnX, 5000, SpawnZ), transform.TransformDirection(-Vector3.up), out hit, Mathf.Infinity, GroundLayer))
+                {
+                    SpawnY = hit.point.y;
+                    Instantiate(ButterFlyPrefab, new Vector3(SpawnX, SpawnY, SpawnX*), Quaternion.identity);
+                }
+                timer = 0;
+                
+            }
+        }
+    }
+
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position,SpawnRadius);
+    }
+
+}
